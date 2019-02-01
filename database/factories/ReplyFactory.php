e3bb12a15caa0a9383f5f6a4845631cf61d05d97
Lvcios <lfloresg0801@gmail.com>
2019-02-01 14:47:01 -0600

    migrations with php artisan

--- database/factories/ReplyFactory.php
+++ database/factories/ReplyFactory.php
@@ -2,8 +2,14 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(App\Reply::class, function (Faker $faker) {
+$factory->define(App\Model\Reply::class, function (Faker $faker) {
     return [
-        //
+        'body' => $faker -> text,
+        'question_id' => function(){
+            return \App\Model\Question::all()->random();
+        },
+        'user_id' =>  function(){
+            return \App\User::all()->random();
+        }
     ];
 });
