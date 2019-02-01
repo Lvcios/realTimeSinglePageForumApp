ba4e28ef8960338186f8f7caab5a3202a6095346
Lvcios <lfloresg0801@gmail.com>
2019-02-01 12:45:06 -0600

    relationships

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,15 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+/*
+use Illuminate\Support\Facades\Schema; para solucionar el problema siguiente
+[Illuminate\Database\QueryException]
+SQLSTATE[42000]: Syntax error or access violation: 1071 Specified key was too long; max key length is 767 bytes (SQL: alter table users add unique users_email_unique(email))
+
+[PDOException]
+SQLSTATE[42000]: Syntax error or access violation: 1071 Specified key was too long; max key length is 767 bytes
+*/
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Schema::defaultStringLength(191);
     }
 
     /**
